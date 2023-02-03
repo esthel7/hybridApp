39f797e95cc8e59d72ba2dfdb5fc6ba071552c7c
esthel <esthel9026@naver.com>
2023-02-03 13:56:38 +0900

    뒤로가기 키 구현

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, BackHandler, Dimensions } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height'; // status bar 높이
 
-const WEBVIEW_URL = "/--url--/";
+const WEBVIEW_URL = "https://kiin.odoc-api.com/";
 const { width: SCREEN_WIDTH } = Dimensions.get("window"); // 화면 크기 알아내도록, width를 screen_width로 사용
 
 export default function App() {
@@ -27,20 +27,18 @@ export default function App() {
       window.ReactNativeWebView.postMessage('navigationStateChange');
     });
   })();
-
   true;`;
+  // post message로 메시지 보내기, 마지막에 true;넣어서 오류 방지
 
   const message = ({ nativeEvent: state }) => {
     if (state.data === 'navigationStateChange') {
-      console.log(state);
-      console.log(state.url);
       setCurrentUrl(state.url);
     }
   };
 
   const onPressHardwareBackButton = () => {
     if (!webViewRef.current) return;
-    if (currentUrl === '/--url--/' || currentUrl === '/--url--/login' || currentUrl === '/--url--/main') {
+    if (currentUrl === 'https://kiin.odoc-api.com/' || currentUrl === 'https://kiin.odoc-api.com/login' || currentUrl === 'https://kiin.odoc-api.com/main') {
       BackHandler.exitApp(); // app 종료
       return;
     }
@@ -63,7 +61,7 @@ export default function App() {
       <WebView style={styles.view} source={{ uri: WEBVIEW_URL }} ref={webViewRef} javaScriptEnabled domStorageEnabled textZoom={100}
         injectedJavaScript={injectedJS} onMessage={message} />
       {/* javascript 사용, 저장소 사용, textzoom값을 지정하여 시스템 각각 다 다른 사이즈 방지 */}
-      {/* onload는 페이지 로드될 경우, onNavigationstatechange는 주소 이동할 때 */}
+      {/* injectedJavaScript는 webview 실행 전 먼저 실행 */}
     </>
   );
 }
