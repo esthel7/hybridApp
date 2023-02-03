3b3d609ab70b7e9aed2c7554e8aec7d0f8d47147
esthel <esthel9026@naver.com>
2023-02-03 13:47:09 +0900

    뒤로가기 키 구현

--- App.js
+++ App.js
@@ -4,23 +4,47 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, BackHandler, Dimensions } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height'; // status bar 높이
 
-const WEBVIEW_URL = "/url 작성/";
+const WEBVIEW_URL = "/--url--/";
 const { width: SCREEN_WIDTH } = Dimensions.get("window"); // 화면 크기 알아내도록, width를 screen_width로 사용
 
 export default function App() {
   const webViewRef = useRef();
-  const [backButtonEnabled, setBackButtonEnabled] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState('');
 
-  const webViewLoaded = () => setBackButtonEnabled(true); // 페이지가 로드되었으므로 변수 true로 설정
-  const onNavigationStateChange = (navState) => setBackButtonEnabled(navState.canGoBack); // 뒤로 갈 수 있는지 확인
+  const injectedJS = `
+  (function() {
+    function wrap(fn) {
+      return function wrapper() {
+        var res = fn.apply(this, arguments);
+        window.ReactNativeWebView.postMessage('navigationStateChange');
+        return res;
+      }
+    }
+
+    history.pushState = wrap(history.pushState);
+    history.replaceState = wrap(history.replaceState);
+    window.addEventListener('popstate', function() {
+      window.ReactNativeWebView.postMessage('navigationStateChange');
+    });
+  })();
+
+  true;`;
+
+  const message = ({ nativeEvent: state }) => {
+    if (state.data === 'navigationStateChange') {
+      console.log(state);
+      console.log(state.url);
+      setCurrentUrl(state.url);
+    }
+  };
 
   const onPressHardwareBackButton = () => {
     if (!webViewRef.current) return;
+    if (currentUrl === '/--url--/' || currentUrl === '/--url--/login' || currentUrl === '/--url--/main') {
+      BackHandler.exitApp(); // app 종료
+      return;
+    }
     else {
-      if (!backButtonEnabled) {
-        BackHandler.exitApp(); // app 종료(뒤로갈 창이 없음)
-        return;
-      }
       webViewRef.current.goBack(); // 웹페이지 뒤로가기
       return true;
     }
@@ -31,13 +55,13 @@ export default function App() {
     return () => {
       BackHandler.removeEventListener('hardwareBackPress', onPressHardwareBackButton);
     };
-  }, [backButtonEnabled]);
+  }, [currentUrl]);
 
   return (
     <>
       <StatusBar style="auto" />
       <WebView style={styles.view} source={{ uri: WEBVIEW_URL }} ref={webViewRef} javaScriptEnabled domStorageEnabled textZoom={100}
-        onLoad={webViewLoaded} onNavigationStateChange={onNavigationStateChange} />
+        injectedJavaScript={injectedJS} onMessage={message} />
       {/* javascript 사용, 저장소 사용, textzoom값을 지정하여 시스템 각각 다 다른 사이즈 방지 */}
       {/* onload는 페이지 로드될 경우, onNavigationstatechange는 주소 이동할 때 */}
     </>
